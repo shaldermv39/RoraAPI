fdbf7c9ec82a1f1042fa66bc74a868ab7da7b51e
shaldermv39 <64363256+shaldermv39@users.noreply.github.com>
2020-05-09 20:04:30 -0500

    Add project files.

---
+++ RoraAPI/Controller/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RoraAPI.Model;
+
+namespace RoraAPI.Controller
+{
+     [ServiceFilter(typeof(UserActivities))]
+    [ApiController]
+    [Route("[controller]/[Action]")] 
+    [Authorize]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        [ActionName("Login")]
+        public IActionResult Login()
+        {
+            return Ok(201);
+        }
+        
+    }
+}
---
+++ RoraAPI/Model/UserActivities.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace RoraAPI.Model
+{
+    public class UserActivities : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var resultContext = await next();
+            var userid = int.Parse(resultContext.HttpContext.User.FindFirst
+                (ClaimTypes.NameIdentifier).Value);
+         //   var repo= resultContext.HttpContext.RequestServices.GetService
+        }
+    }
+}
